ea37a9911d215ad7a6cbc0d7bea1caf2b91f40cd
Naveenchandardev <chandarnaveen88@gmail.com>
2026-04-26 14:51:08 +0530

    #react: edit task

--- REACT/my-react-app/src/Tasks/Todo.jsx
+++ REACT/my-react-app/src/Tasks/Todo.jsx
@@ -3,41 +3,62 @@ import { useState } from "react";
 function Todo() {
   const [task, setTask] = useState("");
   const [list, setList] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
 
-  const addTask = () => {
+  const saveTask = () => {
+    console.log("testing", task);
     if (task.trim() === "") return;
 
-    setList([...list, task]);
+    if (editIndex === null) {
+      setList([...list, task]);
+    } else {
+      const newList = [...list];
+      newList[editIndex] = task;
+      setList(newList);
+      setEditIndex(null);
+    }
+
     setTask("");
   };
 
-  const deleteTask = (deleteIndex) => {
-    const newList = list.filter(
-      (item, index) => index !== deleteIndex
-    );
+  const deleteTask = (i) => {
+    setList(list.filter((item, index) => index !== i));
+  };
 
-    setList(newList);
+  const editTask = (i) => {
+    setTask(list[i]);
+    setEditIndex(i);
   };
 
   return (
     <div className="home-container">
-      <h1>To-Do List</h1>
+      <h1>Todo</h1>
 
       <input
-        type="text"
-        placeholder="Enter task"
         value={task}
-        onChange={(e) => setTask(e.target.value)}
+        onChange={(e) => {
+    
+        setTask(e.target.value);
+      }}
+        placeholder="Enter task"
       />
 
-      <button onClick={addTask}>Add</button>
+      <button onClick={saveTask}>
+        {editIndex === null ? "Add" : "Update"}
+      </button>
 
       <ul>
         {list.map((item, index) => (
           <li key={index}>
             {item}
 
-            <button  style={{ marginLeft: "10px" }}
+            <button   style={{ marginLeft: "8px" }}
+             onClick={() => editTask(index)}>
+              Edit
+            </button>
+
+            <button
+              style={{ marginLeft: "8px" }}
               onClick={() => deleteTask(index)}
             >
               Delete
